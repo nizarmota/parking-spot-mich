20036b6c071418cea49216b781aa420ed3fc9a3c
nizar <nizar@tce.ma.gov.br>
2023-03-04 22:08:05 -0300

    first commit

---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,20 @@
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+
+///Pode ser injetado utilizando um @Autowired, porem foi injetado usando o construtor
+    final ParkingSpotRepository parkingSpotRepository;
+///Na injeção de dependência via construtor, as dependências de uma classe são fornecidas por meio de um construtor. Isso significa que as dependências são passadas como parâmetros no momento da criação do objeto.
+// Essa técnica é muito útil quando uma classe tem várias dependências e pode ajudar a simplificar a criação de objetos e torná-los mais testáveis.
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+
+
+}
